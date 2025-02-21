3246aa129f38d05b809e692f1d6e8076111f1542
Gabriel Machado de Matos - 1292326181 <1292326181@ulife.com.br>
2025-02-21 19:00:06 -0300

    Commit Inicial do Projeto

---
+++ projetoJao/src/main/java/io/github/gabrielmatosprogramador/projetoJao/ProjetoJaoApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielmatosprogramador.projetoJao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJaoApplication.class, args);
+	}
+
+}
